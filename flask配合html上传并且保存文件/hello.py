db10b064687a83a3d4764c84945d9781f4f5981a
e <616625682@qq.com>
2019-01-04 21:25:05 +0800

    修改

--- flask配合html上传并且保存文件/hello.py
+++ flask配合html上传并且保存文件/hello.py
@@ -2,10 +2,10 @@ from flask import Flask,make_response,redirect ,request,render_template
 from werkzeug.utils import secure_filename
 app=Flask(__name__)
 
-@app.route('/upload_file', methods=['GET', 'POST'])
-def upload_file():
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
     if request.method == 'POST':
         f = request.files['file']                #请求到文件
-        f.save('d:\\' + secure_filename(f.filename))   #保存到d盘
+        f.save(secure_filename(f.filename))   #保存到d盘
     return render_template('upfile.html')       
-app.run(port='8888')
+app.run(port='6888')
